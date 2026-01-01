7886783a2ea6dfaad66c7e40a19ed5da19e5ceb6
surendevops75 <surendevops75@gmail.com>
2026-01-01 12:22:19 +0530

    jenkins-shared-library

--- vars/EKSDeploy.groovy
+++ vars/EKSDeploy.groovy
@@ -30,9 +30,11 @@ def call (Map configMap){
                     script{
                         withAWS(region:'us-east-1',credentials:'aws-creds') {
                             sh """
+                                set -e
                                 aws eks update-kubeconfig --region ${REGION} --name ${PROJECT}-${deploy_to}
                                 kubectl get nodes
-                                echo ${deploy_to}, ${appVersion}
+                                sed -i "s/IMAGE_VERSION/${appVersion}/g" values.yaml
+                                helm upgrade --install ${COMPONENT} -f values-${deploy_to}.yaml -n ${PROJECT} --atomic --wait --timeout=5m .
                             """
                         }
                     }
