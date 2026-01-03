47c30456b933b9e9123468c622eacd9083b0423a
surendevops75 <surendevops75@gmail.com>
2026-01-03 12:01:18 +0530

    shipping

--- vars/EKSDeploy.groovy
+++ vars/EKSDeploy.groovy
@@ -34,7 +34,7 @@ def call (Map configMap){
                                 aws eks update-kubeconfig --region ${REGION} --name ${PROJECT}-${deploy_to}
                                 kubectl get nodes
                                 sed -i "s/IMAGE_VERSION/${appVersion}/g" values.yaml
-                                helm upgrade --install ${COMPONENT} -f values-${deploy_to}.yaml -n ${PROJECT} --atomic --wait --timeout=5m .
+                                helm upgrade --install ${COMPONENT} -f values-${deploy_to}.yaml -n ${PROJECT} --atomic --wait --timeout=10m .
                             """
                         }
                     }
