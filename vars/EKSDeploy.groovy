a72fe3ec5d37af320919dae23eccdc7a0ef54a13
surendevops75 <surendevops75@gmail.com>
2026-01-03 13:54:50 +0530

    jenkins-shared-library

--- vars/EKSDeploy.groovy
+++ vars/EKSDeploy.groovy
@@ -99,7 +99,52 @@ def call (Map configMap){
                 cleanWs()
             }
             success {
-                echo 'I will run if success'
+                script {
+                    withCredentials([string(credentialsId: 'slack-token', variable: 'SLACK_WEBHOOK')]) {
+
+                        def payload = """
+                        {
+                        "attachments": [
+                            {
+                            "color": "#2eb886",
+                            "title": "✅ Jenkins Build Successful",
+                            "fields": [
+                                {
+                                "title": "Job Name",
+                                "value": "${env.JOB_NAME}",
+                                "short": true
+                                },
+                                {
+                                "title": "Build Number",
+                                "value": "${env.BUILD_NUMBER}",
+                                "short": true
+                                },
+                                {
+                                "title": "Status",
+                                "value": "SUCCESS",
+                                "short": true
+                                },
+                                {
+                                "title": "Build URL",
+                                "value": "${env.BUILD_URL}",
+                                "short": false
+                                }
+                            ],
+                            "footer": "Jenkins CI",
+                            "ts": ${System.currentTimeMillis() / 1000}
+                            }
+                        ]
+                        }
+                        """
+
+                        sh """
+                        curl -X POST \
+                        -H 'Content-type: application/json' \
+                        --data '${payload}' \
+                        ${SLACK_WEBHOOK}
+                        """
+                    }
+                }
             }
             failure {
                 echo 'I will run if failure'
