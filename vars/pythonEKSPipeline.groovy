0188797b89dc8ebeb7808c16cadf230a1590eb50
surendevops75 <surendevops75@gmail.com>
2026-01-03 15:04:04 +0530

    payment-deploy

--- vars/pythonEKSPipeline.groovy
+++ vars/pythonEKSPipeline.groovy
@@ -127,7 +127,7 @@ def call (Map configMap){
                         withAWS(region:'us-east-1',credentials:'aws-creds') {
                             sh """
                                 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
-                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
+                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} ./
                                 docker images
                                 docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
                             """
